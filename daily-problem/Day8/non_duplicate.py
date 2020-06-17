21a77289312b153cc715db414c836de4600d4d3a
leonardo1101 <leonardo98ti@gmail.com>
2020-06-16 23:20:48 -0300

    Daily problem: Day8 - Find the non-duplicate number using O(1) memory (Fix neg)

--- daily-problem/Day8/non_duplicate.py
+++ daily-problem/Day8/non_duplicate.py
@@ -1,12 +1,31 @@
 def singleNumber(nums):
   nums_len = len(nums)
-  sum_nums = 0
+  sum_pos_pair = 0
+  sum_pos_odd = 0
+  sum_neg_pair = 0
+  sum_neg_odd = 0
   for index in range(nums_len):
-    if sum_nums - nums[index] < 0:
-        sum_nums += nums[index]
+    if nums[index] > 0:
+      if nums[index] % 2:
+        sum_pos_odd += nums[index]
+      else:
+        sum_pos_pair += nums[index]
     else:
-        sum_nums -= nums[index]
-  return sum_nums
+      if abs(nums[index]) % 2:
+        sum_neg_odd += nums[index]
+      else:
+        sum_neg_pair += nums[index]
+  
+  if sum_pos_odd % 2 != 0:
+    return sum_pos_odd % 2
+  elif sum_pos_pair % 4 != 0:
+    return sum_pos_pair % 4
+  elif sum_neg_odd % 2 != 0:
+    return -(sum_neg_odd % 2)
+  elif sum_neg_pair % 4 != 0:
+    return -(sum_neg_pair % 4)
+  else:
+    return 0
 
-print singleNumber([4, 3, 2, 1, 4, 11, 1, 3, 2])
-# 1
+print singleNumber([4, 3, 2, 1, 4, -2, 1, 3, 2])
+
